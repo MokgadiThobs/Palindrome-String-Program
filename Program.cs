0ee5a879043dd6d19a4c6438c21f348d070430c8
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-13 04:37:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_String_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string str = Console.ReadLine();
+
+            bool isPalindrome = str.SequenceEqual(str.Reverse());
+
+            if (isPalindrome)
+            {
+                Console.WriteLine($"{str} is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{str} is not a palindrome.");
+            }
+        }
+    }
+}
